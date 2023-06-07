8f3bb1ba8cbbbe5879e98f70bf668f6d18f752c1
jonsonkurt <55541726+jonsonkurt@users.noreply.github.com>
2023-06-07 15:14:14 +0800

    added bottom navigation

---
+++ lib/view/bottom_navigation_bar.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:salomon_bottom_bar/salomon_bottom_bar.dart';
+import 'home_page.dart';
+import 'org_chart_page.dart';
+import 'professor_page.dart';
+import 'profile_page.dart';
+
+class BottomNavigation extends StatefulWidget {
+  const BottomNavigation({super.key});
+
+  @override
+  State<BottomNavigation> createState() => _BottomNavigationState();
+}
+
+class _BottomNavigationState extends State<BottomNavigation> {
+  var currentIndex = 0;
+  var pages = const [
+    HomePage(),
+    ProfessorPage(),
+    OrgChartPage(),
+    ProfilePage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        bottomNavigationBar: SalomonBottomBar(
+          currentIndex: currentIndex,
+          items: [
+            SalomonBottomBarItem(
+              icon: const Icon(Icons.home),
+              title: const Text("Home"),
+              selectedColor: Colors.pink,
+            ),
+            SalomonBottomBarItem(
+              icon: const Icon(Icons.list),
+              title: const Text("Professor"),
+              selectedColor: Colors.purple,
+            ),
+            SalomonBottomBarItem(
+              icon: const Icon(Icons.people),
+              title: const Text("Org Chart"),
+              selectedColor: Colors.blue,
+            ),
+            SalomonBottomBarItem(
+              icon: const Icon(Icons.person),
+              title: const Text("Profile"),
+              selectedColor: Colors.teal,
+            ),
+          ],
+          onTap: (index) {
+            setState(() {
+              currentIndex = index;
+            });
+          },
+        ),
+        body: pages[currentIndex],
+      ),
+    );
+  }
+}
