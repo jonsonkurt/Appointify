6ce4127122bc5c512c6016dfd44a9a45919780e1
jonsonkurt <55541726+jonsonkurt@users.noreply.github.com>
2023-06-07 15:14:42 +0800

    added welcome page

--- lib/view/welcome_page.dart
+++ lib/view/welcome_page.dart
@@ -1,5 +1,5 @@
+import 'package:appointify/view/sign_in_page.dart';
 import 'package:flutter/material.dart';
-// import 'login_page.dart';
 
 class WelcomePage extends StatelessWidget {
   const WelcomePage({Key? key}) : super(key: key);
@@ -54,28 +54,28 @@ class WelcomePage extends StatelessWidget {
                       const SizedBox(height: 20),
                       OutlinedButton(
                         onPressed: () {
-                          // Navigator.push(
-                          //   context,
-                          //   PageRouteBuilder(
-                          //     pageBuilder:
-                          //         (context, animation, secondaryAnimation) =>
-                          //             const LoginPage(),
-                          //     transitionsBuilder: (context, animation,
-                          //         secondaryAnimation, child) {
-                          //       var begin = const Offset(0.0, 1.0);
-                          //       var end = Offset.zero;
-                          //       var curve = Curves.ease;
+                          Navigator.push(
+                            context,
+                            PageRouteBuilder(
+                              pageBuilder:
+                                  (context, animation, secondaryAnimation) =>
+                                      const SignInPage(),
+                              transitionsBuilder: (context, animation,
+                                  secondaryAnimation, child) {
+                                var begin = const Offset(0.0, 1.0);
+                                var end = Offset.zero;
+                                var curve = Curves.ease;
 
-                          //       var tween = Tween(begin: begin, end: end)
-                          //           .chain(CurveTween(curve: curve));
+                                var tween = Tween(begin: begin, end: end)
+                                    .chain(CurveTween(curve: curve));
 
-                          //       return SlideTransition(
-                          //         position: animation.drive(tween),
-                          //         child: child,
-                          //       );
-                          //     },
-                          //   ),
-                          // );
+                                return SlideTransition(
+                                  position: animation.drive(tween),
+                                  child: child,
+                                );
+                              },
+                            ),
+                          );
                         },
                         style: OutlinedButton.styleFrom(
                           shape: const StadiumBorder(),
