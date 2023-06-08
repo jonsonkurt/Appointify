9d9d0fae8671d16c1bc7c700007a614ba40a5d5a
Jiiroo19 <jhonheron.carsocho@gmail.com>
2023-06-08 23:41:51 +0800

    added upload and retrieve function on profile page

--- lib/view/student/profile_page.dart
+++ lib/view/student/profile_page.dart
@@ -1,24 +1,102 @@
+import 'package:appointify/view/student/storage_upload.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_storage/firebase_storage.dart';
-
+import 'package:file_picker/file_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
 
   @override
   State<ProfilePage> createState() => _ProfilePageState();
-  
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  // final storageRef = FirebaseStorage.instance.ref("");
+  // final mountainsRef = storageRef.child("mountains.jpg");
+  // final imagesRef = storageRef.child("images");
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    final Storage storage = Storage();
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(
-          child: Text("Profile Page"),
-        ),
+        body: Column(children: [
+          Center(
+            child: ElevatedButton(
+              onPressed: () async {
+                final results = await FilePicker.platform.pickFiles(
+                  allowMultiple: false,
+                  type: FileType.custom,
+                  allowedExtensions: ['png', 'jpg'],
+                );
+                if (results == null) {
+                  // ignore: use_build_context_synchronously
+                  ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                    content: Text('No file selected.'),
+                  ));
+                  return;
+                }
+                final String path = results.files.single.path.toString();
+                final String fileName = results.files.single.name;
+
+                storage.uploadFile(path, fileName);
+              },
+              child: const Text('Upload File'),
+            ),
+          ),
+          FutureBuilder(
+              future: storage.listFiles(),
+              builder: (BuildContext context,
+                  AsyncSnapshot<firebase_storage.ListResult> snapshot) {
+                if (snapshot.connectionState == ConnectionState.done &&
+                    snapshot.hasData) {
+                  return Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 30),
+                    height: 50,
+                    child: ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        shrinkWrap: true,
+                        itemCount: snapshot.data!.items.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          return Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: ElevatedButton(
+                              onPressed: () {},
+                              child: Text(snapshot.data!.items[index].name),
+                            ),
+                          );
+                        }),
+                  );
+                }
+                if (snapshot.connectionState == ConnectionState.waiting ||
+                    !snapshot.hasData) {
+                  return CircularProgressIndicator();
+                }
+                return Container();
+              }),
+          FutureBuilder(
+              future: storage.downloadURL('sticker_1.png'),
+              builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+                if (snapshot.connectionState == ConnectionState.done &&
+                    snapshot.hasData) {
+                  return Container(
+                    width: 300,
+                    height: 250,
+                    child: Image.network(
+                      snapshot.data!, 
+                      fit: BoxFit.cover,
+                    ),
+                  );
+                }
+                if (snapshot.connectionState == ConnectionState.waiting ||
+                    !snapshot.hasData) {
+                  return CircularProgressIndicator();
+                }
+                return Container();
+              })
+        ]),
       ),
     );
   }
