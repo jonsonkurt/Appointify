4babecafc7d35e63e4316c5bfb160a4a883cd011
Jiiroo19 <jhonheron.carsocho@gmail.com>
2023-06-09 21:00:12 +0800

    added profilePicStatus and UID to initial data after sign up

--- lib/view/student/profile_controller.dart
+++ lib/view/student/profile_controller.dart
@@ -1,10 +1,8 @@
 import 'dart:io';
-
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
 
 class ProfileController with ChangeNotifier {
