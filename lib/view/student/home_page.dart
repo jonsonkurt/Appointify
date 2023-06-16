9a6572a3bb4d4170849a03248576c7ed0db2ff81
DeVeraGiovanni <giovanni.devera@cvsu.edu.ph>
2023-06-16 19:15:01 +0800

    removing unnecessary package

--- lib/view/student/home_page.dart
+++ lib/view/student/home_page.dart
@@ -4,12 +4,10 @@ import 'package:appointify/view/student/profile_controller.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/ui/firebase_animated_list.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:logger/logger.dart';
 import 'package:contained_tab_bar_view/contained_tab_bar_view.dart';
-import 'package:material_dialogs/material_dialogs.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
