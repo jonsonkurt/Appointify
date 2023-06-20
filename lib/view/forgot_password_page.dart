fe0bf2c88cf65941a75e867c7eb8051e9419b901
DavePresbitero <davepresbitero03@gmail.com>
2023-06-20 14:40:59 +0800

    forgot pass and profile container color update UI

--- lib/view/forgot_password_page.dart
+++ lib/view/forgot_password_page.dart
@@ -1,3 +1,4 @@
+import 'package:appointify/view/sign_in_page.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -20,23 +21,84 @@ class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SizedBox(
-        height: MediaQuery.of(context).size.height,
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const SizedBox(height: 30),
-              const Text(
-                "Forgot Password",
-                style: TextStyle(
-                  fontFamily: "GothamRnd-Bold",
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
+      backgroundColor: const Color(0xFFF2F2F2),
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(
+              height: kToolbarHeight,
+              child: PreferredSize(
+                preferredSize: const Size.fromHeight(kToolbarHeight),
+                child: Container(
+                  color: const Color(0xFFFF9343),
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  alignment: Alignment.centerLeft,
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const SignInPage()),
+                      );
+                    },
+                    child: const Icon(Icons.arrow_back, color: Colors.white),
+                  ),
                 ),
               ),
-              const SizedBox(height: 20),
-              Column(
+            ),
+            Stack(children: [
+              Container(
+                width: double.infinity,
+                height: MediaQuery.of(context).size.height / 5,
+                decoration: const BoxDecoration(
+                  color: Color(0xFFFF9343),
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(30.0),
+                    bottomRight: Radius.circular(30.0),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              Container(
+                alignment: Alignment.center,
+                width: MediaQuery.of(context).size.width,
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: const SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        "Forgot Password",
+                        style: TextStyle(
+                          fontFamily: "GothamRnd-Bold",
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Divider(
+                        color: Colors.white,
+                        thickness: 2,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ]),
+            const SizedBox(
+              height: 50,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(25),
+              child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   const Text(
@@ -69,36 +131,31 @@ class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
                   ),
                 ],
               ),
-              const SizedBox(height: 15),
-              Center(
-                child: ElevatedButton(
-                  onPressed: () async {
-                    String email = _emailController.text;
-
-                    await FirebaseAuth.instance
-                        .sendPasswordResetEmail(email: email);
-
-                    _emailController.clear();
-                  },
-                  style: ElevatedButton.styleFrom(
-                    fixedSize: const Size(203, 50),
-                    backgroundColor: const Color(0xFFFF9343),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(
-                          10), // Adjust the radius as needed
-                    ),
-                  ),
-                  child: const Text(
-                    'Forgot',
-                    style: TextStyle(
-                      fontFamily: "GothamRnd-Medium.otf",
-                      fontSize: 20,
-                    ),
-                  ),
+            ),
+            const SizedBox(height: 25),
+            ElevatedButton(
+              onPressed: () async {
+                String email = _emailController.text;
+                await FirebaseAuth.instance
+                    .sendPasswordResetEmail(email: email);
+                _emailController.clear();
+              },
+              style: ElevatedButton.styleFrom(
+                fixedSize: const Size(203, 50),
+                backgroundColor: const Color(0xFFFF9343),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
                 ),
               ),
-            ],
-          ),
+              child: const Text(
+                'Send Link',
+                style: TextStyle(
+                  fontFamily: "GothamRnd-Medium.otf",
+                  fontSize: 20,
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
