7e5f68b97bec8cd4c36d1a4f6262dbc3feb06d86
DeVeraGiovanni <giovanni.devera@cvsu.edu.ph>
2023-06-22 17:46:34 +0800

    added a schoolOrg value

--- lib/view/student/org_chart_page.dart
+++ lib/view/student/org_chart_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:graphview/GraphView.dart';
+import 'package:firebase_database/firebase_database.dart';
 
 class OrgChartPage extends StatefulWidget {
   const OrgChartPage({super.key});
@@ -12,121 +13,292 @@ class OrgChartPage extends StatefulWidget {
 class _OrgChartPage extends State<OrgChartPage> {
   var json = {
     'nodes': [
-      {'id': 1, 'label': 'President'},
-      {'id': 2, 'label': 'Vice President'},
-      {'id': 3, 'label': 'Secretary'},
-      {'id': 4, 'label': 'Auditor'},
-      {'id': 5, 'label': 'Treasurer'},
-      {'id': 6, 'label': 'dot'},
-      {'id': 7, 'label': 'square'},
-      {'id': 8, 'label': 'triangle'},
+      {
+        "id": 11,
+        "label": "Ar. Brian",
+        "rank": 6,
+      },
+      {
+        "id": 12,
+        "label": "Engr. Alvin",
+        "rank": 5,
+      },
+      {
+        "id": 13,
+        "label": "Engr. Marcelino",
+        "rank": 5,
+      },
+      {
+        "id": 14,
+        "label": "Engr. Ralph",
+        "rank": 5,
+      },
+      {
+        "id": 15,
+        "label": "Prof. William",
+        "rank": 5,
+      },
+      {
+        "id": 1,
+        "label": "Robles",
+        "rank": 1,
+      },
+      {
+        "id": 2,
+        "label": "Dr. Ma. Agnes",
+        "rank": 2,
+      },
+      {
+        "id": 3,
+        "label": "Dr. Willie",
+        "rank": 3,
+      },
+      {
+        "id": 4,
+        "label": "Engr. Roslyn",
+        "rank": 4,
+      },
+      {
+        "id": 5,
+        "label": "Engr. Larry",
+        "rank": 5,
+      },
+      {
+        "id": 6,
+        "label": "Engr. Cene",
+        "rank": 5,
+      },
+      {
+        "id": 7,
+        "label": "Engr. Renato",
+        "rank": 5,
+      },
+      {
+        "id": 8,
+        "label": "Ar. Kenn",
+        "rank": 5,
+      },
+      {
+        "id": 9,
+        "label": "Ar. Christian",
+        "rank": 6,
+      },
+      {
+        "id": 10,
+        "label": "Ar. Dan",
+        "rank": 6,
+      },
     ],
     'edges': [
-      {'from': 1, 'to': 2},
-      {'from': 2, 'to': 3},
-      {'from': 2, 'to': 4},
-      {'from': 2, 'to': 5},
-      {'from': 5, 'to': 6},
-      {'from': 5, 'to': 7},
-      {'from': 6, 'to': 8}
+      {"from": 1, "to": 2},
+      {"from": 2, "to": 3},
+      {"from": 3, "to": 4},
+      {"from": 4, "to": 12},
+      {"from": 4, "to": 13},
+      {"from": 4, "to": 14},
+      {"from": 4, "to": 15},
+      {"from": 4, "to": 5},
+      {"from": 4, "to": 6},
+      {"from": 4, "to": 7},
+      {"from": 4, "to": 8},
+      {"from": 8, "to": 11},
+      {"from": 8, "to": 9},
+      {"from": 8, "to": 10}
     ]
   };
 
+  var schoolOrg = {"nodes": [], "edges": []};
+
   @override
   Widget build(BuildContext context) {
+    DatabaseReference ref =
+        FirebaseDatabase.instance.ref().child('organizationChart');
+
     return SafeArea(
       child: Scaffold(
           appBar: AppBar(
             title: const Text("Org Chart"),
             backgroundColor: Colors.orange,
           ),
-          body: SafeArea(
-            child: Column(
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                Wrap(
-                  children: [
-                    SizedBox(
-                      width: 100,
-                      child: TextFormField(
-                        initialValue: builder.siblingSeparation.toString(),
-                        decoration: const InputDecoration(
-                            labelText: 'Sibling Separation'),
-                        onChanged: (text) {
-                          builder.siblingSeparation = int.tryParse(text) ?? 100;
-                          setState(() {});
-                        },
-                      ),
+          body: StreamBuilder(
+            stream: ref.orderByChild("rank").onValue,
+            builder: (context, AsyncSnapshot snapshot) {
+              dynamic values;
+              String latestRank = "";
+              if (!snapshot.hasData) {
+                return const Center(child: CircularProgressIndicator());
+              } else if (snapshot.hasData) {
+                DataSnapshot dataSnapshot = snapshot.data!.snapshot;
+                if (dataSnapshot.value != null) {
+                  values = dataSnapshot.value;
+
+                  for (int index = 0; index < values.length; index++) {
+                    if (values.keys.elementAt(index) != "null") {
+                      String id = values.keys.elementAt(index);
+                      String name = values[id]["name"];
+                      String rank = values[id]["rank"];
+                      int intRank = int.parse(rank);
+                      var newNode = {
+                        "id": id,
+                        "label": name,
+                        "rank": intRank,
+                      };
+                      schoolOrg["nodes"]!.add(newNode);
+                    }
+                  }
+                }
+                var jsonNodes = schoolOrg["nodes"];
+
+                Map<int, List<Map<String, dynamic>>> groupedData = {};
+                for (var entry in jsonNodes!) {
+                  int rank = entry["rank"];
+
+                  if (!groupedData.containsKey(rank)) {
+                    groupedData[rank] = [];
+                  }
+                  groupedData[rank]!.add(entry);
+                }
+
+                // Print the ranks and corresponding IDs and labels
+                for (int rank = 1; rank <= 5; rank++) {
+                  if (groupedData.containsKey(rank) &&
+                      groupedData.containsKey(rank + 1)) {
+                    List<Map<String, dynamic>> rankData1 = groupedData[rank]!;
+                    List<Map<String, dynamic>> rankData2 =
+                        groupedData[rank + 1]!;
+                    List<String> entries = [];
+
+                    for (var entry1 in rankData1) {
+                      for (var entry2 in rankData2) {
+                        if (rank == 5) {
+                          var newEdges = {
+                            "from": entry1["id"],
+                            "to": entry2["id"]
+                          };
+                          if (entry1["label"] == "Ar. Kenn") {
+                            schoolOrg["edges"]!.add(newEdges);
+                          }
+                        } else {
+                          var newEdges = {
+                            "from": entry1["id"],
+                            "to": entry2["id"]
+                          };
+                          schoolOrg["edges"]!.add(newEdges);
+                        }
+                      }
+                    }
+                  }
+                }
+                print("NODES: ${schoolOrg["nodes"]}");
+                print("EDGES: ${schoolOrg["edges"]}");
+
+                // return Card();
+                return SafeArea(
+                  child: Center(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        Wrap(
+                          children: [
+                            SizedBox(
+                              width: 100,
+                              child: TextFormField(
+                                initialValue:
+                                    builder.siblingSeparation.toString(),
+                                decoration: const InputDecoration(
+                                    labelText: 'Sibling Separation'),
+                                onChanged: (text) {
+                                  builder.siblingSeparation =
+                                      int.tryParse(text) ?? 100;
+                                  setState(() {});
+                                },
+                              ),
+                            ),
+                            SizedBox(
+                              width: 100,
+                              child: TextFormField(
+                                initialValue:
+                                    builder.levelSeparation.toString(),
+                                decoration: const InputDecoration(
+                                    labelText: 'Level Separation'),
+                                onChanged: (text) {
+                                  builder.levelSeparation =
+                                      int.tryParse(text) ?? 100;
+                                  setState(() {});
+                                },
+                              ),
+                            ),
+                            SizedBox(
+                              width: 100,
+                              child: TextFormField(
+                                initialValue:
+                                    builder.subtreeSeparation.toString(),
+                                decoration: const InputDecoration(
+                                    labelText: 'Subtree separation'),
+                                onChanged: (text) {
+                                  builder.subtreeSeparation =
+                                      int.tryParse(text) ?? 100;
+                                  setState(() {});
+                                },
+                              ),
+                            ),
+                            SizedBox(
+                              width: 100,
+                              child: TextFormField(
+                                initialValue: builder.orientation.toString(),
+                                decoration: const InputDecoration(
+                                    labelText: 'Orientation'),
+                                onChanged: (text) {
+                                  builder.orientation =
+                                      int.tryParse(text) ?? 100;
+                                  setState(() {});
+                                },
+                              ),
+                            ),
+                          ],
+                        ),
+                        Expanded(
+                          child: InteractiveViewer(
+                              constrained: false,
+                              boundaryMargin: const EdgeInsets.all(50),
+                              minScale: 0.1,
+                              maxScale: 5.6,
+                              child: GraphView(
+                                graph: graph,
+                                algorithm: BuchheimWalkerAlgorithm(
+                                    builder, TreeEdgeRenderer(builder)),
+                                paint: Paint()
+                                  ..color = Colors.black
+                                  ..strokeWidth = 1
+                                  ..style = PaintingStyle.stroke,
+                                builder: (Node node) {
+                                  // I can decide what widget should be shown here based on the id
+                                  var a = node.key!.value as int?;
+                                  var nodes = json['nodes']!;
+
+                                  var nodeValue = nodes.firstWhere(
+                                      (element) => element["id"] == a);
+
+                                  return rectangleWidget(
+                                    nodeValue["label"] as String?,
+                                  );
+                                },
+                              )),
+                        ),
+                      ],
                     ),
-                    SizedBox(
-                      width: 100,
-                      child: TextFormField(
-                        initialValue: builder.levelSeparation.toString(),
-                        decoration: const InputDecoration(
-                            labelText: 'Level Separation'),
-                        onChanged: (text) {
-                          builder.levelSeparation = int.tryParse(text) ?? 100;
-                          setState(() {});
-                        },
-                      ),
-                    ),
-                    SizedBox(
-                      width: 100,
-                      child: TextFormField(
-                        initialValue: builder.subtreeSeparation.toString(),
-                        decoration: const InputDecoration(
-                            labelText: 'Subtree separation'),
-                        onChanged: (text) {
-                          builder.subtreeSeparation = int.tryParse(text) ?? 100;
-                          setState(() {});
-                        },
-                      ),
-                    ),
-                    SizedBox(
-                      width: 100,
-                      child: TextFormField(
-                        initialValue: builder.orientation.toString(),
-                        decoration:
-                            const InputDecoration(labelText: 'Orientation'),
-                        onChanged: (text) {
-                          builder.orientation = int.tryParse(text) ?? 100;
-                          setState(() {});
-                        },
-                      ),
-                    ),
-                  ],
-                ),
-                Expanded(
-                  child: InteractiveViewer(
-                      constrained: false,
-                      boundaryMargin: const EdgeInsets.all(100),
-                      minScale: 0.01,
-                      maxScale: 5.6,
-                      child: GraphView(
-                        graph: graph,
-                        algorithm: BuchheimWalkerAlgorithm(
-                            builder, TreeEdgeRenderer(builder)),
-                        paint: Paint()
-                          ..color = Colors.black
-                          ..strokeWidth = 1
-                          ..style = PaintingStyle.stroke,
-                        builder: (Node node) {
-                          // I can decide what widget should be shown here based on the id
-                          var a = node.key!.value as int?;
-                          var nodes = json['nodes']!;
-                          var nodeValue =
-                              nodes.firstWhere((element) => element['id'] == a);
-                          return rectangleWidget(nodeValue['label'] as String?);
-                        },
-                      )),
-                ),
-              ],
-            ),
+                  ),
+                );
+              }
+              return const Text("hello");
+            },
           )),
     );
   }
 
-  Widget rectangleWidget(String? a) {
+  Widget rectangleWidget(
+    String? a,
+  ) {
     return InkWell(
       onTap: () {
         print('clicked');
@@ -148,10 +320,12 @@ class _OrgChartPage extends State<OrgChartPage> {
 
   @override
   void initState() {
-    var edges = json['edges']!;
+    super.initState();
+    var edges = json["edges"]!;
     edges.forEach((element) {
-      var fromNodeId = element['from'];
-      var toNodeId = element['to'];
+      var fromNodeId = element["from"];
+      var toNodeId = element["to"];
+
       graph.addEdge(Node.Id(fromNodeId), Node.Id(toNodeId));
     });
 
